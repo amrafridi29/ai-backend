29ea7bbbc1389ed6f5c8a014e97ec78b11624a8e
Muhammad Amir <amrafridi.29@gmail.com>
2025-12-18 01:06:18 +0500

    add observability

--- src/modules/ai/mastra/index.ts
+++ src/modules/ai/mastra/index.ts
@@ -3,13 +3,27 @@ import { weatherAgent } from './agents/weather-agent';
 import { nameFormattingWorkflow } from './workflows/name-formatting-workflow';
 import { LibSQLStore } from '@mastra/libsql';
 import { PinoLogger } from '@mastra/loggers';
+import {
+  CloudExporter,
+  DefaultExporter,
+  SamplingStrategyType,
+  SensitiveDataFilter,
+} from '@mastra/core/ai-tracing';
 
 export const mastra = new Mastra({
   agents: { weatherAgent },
   workflows: { nameFormattingWorkflow },
   observability: {
-    default: { enabled: true }, // Enables DefaultExporter and CloudExporter
+    configs: {
+      default: {
+        serviceName: 'mastra',
+        sampling: { type: SamplingStrategyType.ALWAYS },
+        processors: [new SensitiveDataFilter()],
+        exporters: [new CloudExporter(), new DefaultExporter()],
+      },
+    },
   },
+
   storage: new LibSQLStore({
     url: 'file:./mastra.db', // Storage is required for tracing
   }),
