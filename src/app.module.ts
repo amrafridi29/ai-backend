1bbbe167053a862650a625715ddd2db5461423b3
Muhammad Amir <amrafridi.29@gmail.com>
2025-12-31 06:37:45 +0500

    add postgres db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AiModule } from './modules/ai/ai.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // ❗ disable in production
     }),
     AiModule,
   ],
